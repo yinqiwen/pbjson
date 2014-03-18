65c721fef93fb2ee8de17c75e359f5cc49fad6e7
yinqiwen <yinqiwen@gmail.com>
2014-03-18 11:18:38 +0800

    fix for optional field

--- src/pbjson.cpp
+++ src/pbjson.cpp
@@ -1,32 +1,9 @@
 /*
- *Copyright (c) 2013-2014, yinqiwen <yinqiwen@gmail.com>
- *All rights reserved.
+ * pbjson.cpp
  *
- *Redistribution and use in source and binary forms, with or without
- *modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of Redis nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- *THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
- *BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- *THE POSSIBILITY OF SUCH DAMAGE.
+ *  Created on: Jan 26, 2014
+ *      Author: wangqiying
  */
-
 #include "pbjson.hpp"
 #include "bin2ascii.h"
 #include "rapidjson/rapidjson.h"
@@ -46,6 +23,7 @@ namespace pbjson
     {
         const Reflection *ref = msg->GetReflection();
         const bool repeated = field->is_repeated();
+
         size_t array_size = 0;
         if (repeated)
         {
@@ -247,9 +225,16 @@ namespace pbjson
             if (!ref)
                 return NULL;
             const char *name = field->name().c_str();
-            rapidjson::Value* field_json = filed2json(msg, field, allocator);
-            root->AddMember(name, *field_json, allocator);
-            delete field_json;
+            if (field->is_optional() && !ref->HasField(*msg, field))
+            {
+                //do nothing
+            }
+            else
+            {
+                rapidjson::Value* field_json = filed2json(msg, field, allocator);
+                root->AddMember(name, *field_json, allocator);
+                delete field_json;
+            }
         }
         return root;
     }
